9e1d50031cc2c7cbdd45a932d1ba762b2a336412
Andreas Bresser <self@andreasbresser.de>
2020-05-05 15:14:23 +0200

    initial content

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Home assistent development environment
-Docker based home assistent development environment to create HACS-Apps (travis-ci for integration testing)
+
+Docker based home assistent development environment to create Hass-Components (travis-ci for integration testing).
+
+## Overview
+
+This system consists of 3 docker container:
+
+- **home_assistant** container for a home assistant instance
+- **nginx** a webserver to allow access to home-assistant from the host system.
+- **mqtt** a mosquitto-server (for testing).
+
+## Usage
+
+This example system tests the integration of a simple sensor. The code for this is based on <https://github.com/home-assistant/example-custom-config/tree/master/custom_components/example_sensor> its code can be found in `example_sensor/` which is mounted to `/config/custom_components/example_sensor` in the `docker-compose.yml`-file
+
+## Manual-testing
+
+Run `docker-compose up` to start home-assistant behind nginx and mqtt as development setup.
+
+The home-assistant config comes with a user for the web interface named `user`, his password is `1234`.
+It also includes a basic setup of MQTT using an own mosquitto instance.
+
+You can access the web interface for manual testing on [http://localhost](http://localhost), assuming you havn't changed anything and run nginx on port 80 - feel free to change that in the `docker-compose.yml`.
+
+## Integration-Testing
+
+to run the integration tests just run `docker-compose run test`.
+
+This will also be run by travis - take a look at the `.travis-ci.yml` for details.
---
+++ test/test_sensor.py
@@ -0,0 +1,97 @@
+# we use the normal Web Interface to logg in and execute our tests
+# alternative would be: https://www.home-assistant.io/integrations/rest/
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+LOVELACE_USERNAME='user'
+LOVELACE_PASSWORD='1234'
+# This should be the nginx-docker container name, see docker-compose file.
+LOGIN_URL='http://nginx'
+HASS_STARTUP_TIME=1
+
+
+def setup():
+    """Start headless Chrome in docker container."""
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+    # wait for home-assistant to start up
+    driver.implicitly_wait(HASS_STARTUP_TIME)
+    return driver
+
+
+def expand_shadow_element(driver, element):
+    """Small workaround to get the shadowRoot element using JS."""
+    return driver.execute_script('return arguments[0].shadowRoot', element)
+
+
+def get_from_shadow(driver, elem, qry):
+    for q in qry:
+        elem = elem.find_element_by_css_selector(q)
+        elem = expand_shadow_element(driver, elem)
+    return elem
+
+
+def login(driver, user=LOVELACE_USERNAME, passwd=LOVELACE_PASSWORD):
+    """execute login on main page."""
+    driver.get(LOGIN_URL)
+    qry = [
+        'ha-authorize',
+        'ha-auth-flow',
+        'ha-form'
+    ]
+    ha_form = get_from_shadow(driver, driver, qry)
+
+    inner_forms = ha_form.find_elements_by_tag_name('ha-form')
+    assert len(inner_forms) == 2
+    user_form, pw_form = inner_forms
+
+    qry = ['ha-form-string']
+    user_form = expand_shadow_element(driver, user_form)
+    user_input = get_from_shadow(driver, user_form, qry)
+    user_input = user_input.find_element_by_css_selector('paper-input')
+    user_input.send_keys(LOVELACE_USERNAME)
+
+    pw_form = expand_shadow_element(driver, pw_form)
+    pass_input = get_from_shadow(driver, pw_form, qry)
+    pass_input = pass_input.find_element_by_css_selector('paper-input')
+    pass_input.send_keys(LOVELACE_PASSWORD)
+
+    pass_input.send_keys(Keys.RETURN)
+
+
+def test_sensor():
+    driver = setup()
+    login(driver)
+
+    qry = [
+        'home-assistant',
+        'home-assistant-main',
+        'app-drawer-layout partial-panel-resolver ha-panel-lovelace',
+        'hui-root',
+        'ha-app-layout#layout div#view hui-view'
+    ]
+    hui_view = get_from_shadow(driver, driver, qry)
+    badges_container = hui_view.find_element_by_css_selector('div#badges')
+    badges = badges_container.find_elements_by_css_selector('hui-state-label-badge')
+
+    found_sensor = None
+
+    for badge in badges:
+        badge = expand_shadow_element(driver, badge)
+        badge_label = get_from_shadow(driver, badge, ['ha-state-label-badge'])
+        sensor = badge_label.find_elements_by_css_selector('ha-label-badge.sensor')
+        if not sensor:
+            continue
+
+        sensor = expand_shadow_element(driver, sensor[0])
+        sensor_value = sensor.find_element_by_css_selector('div.value span')
+        found_sensor = int(sensor_value.text) == 23
+
+    assert found_sensor, 'sensor not found in badges'
+    
+
